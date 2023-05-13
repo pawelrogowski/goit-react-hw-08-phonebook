e9afee326f3425d70c7cfebd971dd210675fcd5c
Feiron <pawel.rogowski.praca@gmail.com>
2023-05-13 15:46:24 +0200

    code cleaning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Routes, Navigate, Outlet } from 'react-router-dom';
-// Import the Header, Notification, and Auth components as before
 import Header from './Header/Header';
 import Notification from './Notification/Notification';
 import Auth from './Auth/Auth';
 import LoadingSpinner from './LoadingSpinner/LoadingSpinner';
 
-// Lazy load the Register component
 const Register = lazy(() => import('./Register/Register'));
 
 function App() {
